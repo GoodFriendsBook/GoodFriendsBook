f267c5c7d843bf6a0de1d3986541b488e513ab55
GoodFriendsBook <linuxprobusiness@yahoo.com>
2025-05-18 22:27:35 -0400

    Add files via upload

---
+++ www/html/GoodFriendsBook.com/upload_cgi.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+import os
+import sys
+
+
+
+
+
+
+html = ''
+
+html += f"""
+<html>
+   <body>
+      <h1>UPLOAD FILE</h1>
+      <form enctype = "multipart/form-data" action = "https://localhost/ajax/www_upload_get_cgi.py" method = "post">
+      <p>File: <input type = "file" name = "filename" /></p>
+      <p><input type = "submit" value = "Upload" /></p>
+      </form>
+   </body>
+</html>
+"""
+
+#print( session_cookie_value )
+print("Content-Type:text/html;charset=utf-8;")
+print()
+print(html)
