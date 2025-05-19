89094508b364ede02de0e9d435898cadcea35c99
GoodFriendsBook <linuxprobusiness@yahoo.com>
2025-05-18 22:19:35 -0400

    Update upload_cgi.py

--- www/html/GoodFriendsBook.com/upload_cgi.py
+++ www/html/GoodFriendsBook.com/upload_cgi.py
@@ -15,7 +15,7 @@ html += f"""
 <html>
    <body>
       <h1>UPLOAD FILE</h1>
-      <form enctype = "multipart/form-data" action = "https://localhost/ajax/upload_get_cgi.py" method = "post">
+      <form enctype = "multipart/form-data" action = "https://localhost/ajax/www_upload_get_cgi.py" method = "post">
       <p>File: <input type = "file" name = "filename" /></p>
       <p><input type = "submit" value = "Upload" /></p>
       </form>
